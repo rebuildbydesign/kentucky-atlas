d0645bf3c5ee908a5adefe42e083c8a1ddf4be2d
Rebuild by Design <jhuynh@rebuildbydesign.org>
2025-01-22 14:10:27 -0500

    verson 1 upload

---
+++ scripts.js
@@ -0,0 +1,178 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoiajAwYnkiLCJhIjoiY2x1bHUzbXZnMGhuczJxcG83YXY4czJ3ayJ9.S5PZpU9VDwLMjoX_0x5FDQ'; // Your Mapbox access token
+
+var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/j00by/cm689ppsp00jt01s7ddjd69b6', // Use a Mapbox base style
+    center: [-85.7682, 37.8393], // Centered on Kentucky
+    zoom: 6.5
+});
+
+map.on('load', function () {
+    addLayers();
+    handleMapClick();
+});
+
+function addLayers() {
+    // FEMA Disaster Declarations by County Level
+    map.addSource('kentuckyFema', {
+        type: 'geojson',
+        data: 'data/KY_FEMA_County.json'
+    });
+
+    map.addLayer({
+        'id': 'femaDisasters',
+        'type': 'fill',
+        'source': 'kentuckyFema',
+        'paint': {
+            'fill-color': [
+                'match',
+                ['to-number', ['get', 'COUNTY_DIS'], 0],
+                0, '#ffffff', 1, '#fee5d9', 2, '#fee5d9',
+                3, '#fcae91', 4, '#fcae91', 5, '#fb6a4a',
+                6, '#fb6a4a', 7, '#de2d26', 8, '#de2d26',
+                9, '#de2d26', 10, '#a50f15', 11, '#a50f15',
+                12, '#a50f15', 13, '#a50f15', 14, '#a50f15',
+                15, '#a50f15', '#ffffff'
+            ],
+            'fill-opacity': 1
+        }
+    });
+
+    addCongressionalLayers();
+    addHouseLayers();
+    addSenateLayers();
+}
+
+function addCongressionalLayers() {
+    map.addSource('kyCongress', {
+        type: 'geojson',
+        data: 'data/KY_Congress.json'
+    });
+
+    map.addLayer({
+        'id': 'congressionalDistricts',
+        'type': 'fill',
+        'source': 'kyCongress',
+        'paint': {
+            'fill-color': 'transparent',
+            'fill-outline-color': '#000000'
+        }
+    });
+}
+
+function addHouseLayers() {
+    map.addSource('kyHouse', {
+        type: 'geojson',
+        data: 'data/KY_House.json'
+    });
+
+    map.addLayer({
+        'id': 'houseDistricts',
+        'type': 'fill',
+        'source': 'kyHouse',
+        'paint': {
+            'fill-color': 'transparent',
+            'fill-outline-color': '#000000'
+        }
+    });
+}
+
+function addSenateLayers() {
+    map.addSource('kySenate', {
+        type: 'geojson',
+        data: 'data/KY_Senate.json'
+    });
+
+    map.addLayer({
+        'id': 'senateDistricts',
+        'type': 'fill',
+        'source': 'kySenate',
+        'paint': {
+            'fill-color': 'transparent',
+            'fill-outline-color': '#000000'
+        }
+    });
+}
+
+function handleMapClick() {
+    map.on('click', function (e) {
+        var features = map.queryRenderedFeatures(e.point, {
+            layers: ['femaDisasters', 'congressionalDistricts', 'houseDistricts', 'senateDistricts']
+        });
+
+        if (features.length > 0) {
+            var featureData = consolidateFeatureData(features);
+            var popupContent = createPopupContent(featureData);
+            showPopup(e.lngLat, popupContent);
+        }
+    });
+}
+
+function consolidateFeatureData(features) {
+    var featureData = {
+        countyName: '',
+        disasters: '',
+        femaObligations: '',
+        countyPopulation: '',
+        countyPerCapita: '',
+        congressionalDist: '',
+        congressRepName: '',
+        houseDist: '',
+        houseRepName: '',
+        senateDist: '',
+        senateRepName: ''
+    };
+
+    features.forEach(function(feature) {
+        switch (feature.layer.id) {
+            case 'femaDisasters':
+                featureData.countyName = feature.properties.NAME;
+                featureData.disasters = feature.properties.COUNTY_DIS;
+                featureData.femaObligations = feature.properties.COUNTY_FEM;
+                featureData.countyPopulation = feature.properties.COUNTY_POP;
+                featureData.countyPerCapita = feature.properties.COUNTY_PER;
+                break;
+            case 'congressionalDistricts':
+                featureData.congressionalDist = feature.properties.OFFICE_ID;
+                featureData.congressRepName = feature.properties.FIRSTNAME + ' ' + feature.properties.LASTNAME;
+                break;
+            case 'houseDistricts':
+                featureData.houseDist = feature.properties.District;
+                featureData.houseRepName = feature.properties.Legislator;
+                break;
+            case 'senateDistricts':
+                featureData.senateDist = feature.properties.District;
+                featureData.senateRepName = feature.properties.Legislator;
+                break;
+        }
+    });
+
+    return featureData;
+}
+
+function createPopupContent(featureData) {
+    return `
+       <div style="color: #000;">
+    <h2 style="color: #a50f15;">${featureData.countyName} County</h2>
+    <strong>Federal Disaster Declarations:</strong> ${featureData.disasters || 'N/A'}<br>
+    <strong>FEMA Obligations (PA+HM):</strong> ${featureData.femaObligations ? `${parseFloat(featureData.femaObligations).toLocaleString('en-US', { style: 'currency', currency: 'USD', minimumFractionDigits: 2 })}` : 'N/A'}<br>
+    <strong>County Population:</strong> ${featureData.countyPopulation ? parseInt(featureData.countyPopulation).toLocaleString('en-US') : 'N/A'}<br>
+    <strong>County Per Capita:</strong> ${featureData.countyPerCapita ? `${parseFloat(featureData.countyPerCapita).toLocaleString('en-US', { style: 'currency', currency: 'USD', minimumFractionDigits: 2 })}` : 'N/A'}
+    <h2 style="color: #a50f15;">Legislative Members</h2>
+    <strong>Congress:</strong> ${featureData.congressRepName || 'N/A'} (${featureData.congressionalDist || 'N/A'})<br>
+    <strong>House of Representative:</strong> ${featureData.houseRepName || 'N/A'} (${featureData.houseDist || 'N/A'})<br>
+    <strong>Senate:</strong> ${featureData.senateRepName || 'N/A'} (${featureData.senateDist || 'N/A'})
+    <p style="color: gray; font-style: italic; font-size: 0.9em;">* <a href="https://rebuildbydesign.org/atlas-of-accountability/" target="_blank" style="color: gray;">Atlas of Accountability (2011-2023) by Rebuild by Design</a></p>
+
+</div>
+
+
+    `;
+}
+
+function showPopup(lngLat, content) {
+    new mapboxgl.Popup()
+        .setLngLat(lngLat)
+        .setHTML(content)
+        .addTo(map);
+}
